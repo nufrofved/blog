6f30b3864a95b4a27debc70a1adc527db052bb65
nufrofved <nufrofved@gmail.com>
2024-12-23 14:38:01 +0900

    velite 설치, 환경설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { posts } from '@/.velite';
+
 // TODO: Add posts maximally 10 posts
 export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
+      {posts.map(({ title }) => (
+        <>{title}</>
+      ))}
     </>
   );
 }
