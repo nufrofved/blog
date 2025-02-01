b736a18907c23800a1fc6d79ea69253dba26a387
nufrofved <nufrofved@gmail.com>
2025-02-01 23:51:38 +0900

    Add styling to MDX components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,117 @@
-import type { MDXComponents } from 'mdx/types';
+import React, { ComponentPropsWithoutRef } from 'react';
 
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components,
-  };
+import Link from 'next/link';
+import { MDXComponents } from 'mdx/types';
+
+type HeadingProps = ComponentPropsWithoutRef<'h1'>;
+type ParagraphProps = ComponentPropsWithoutRef<'p'>;
+type ListProps = ComponentPropsWithoutRef<'ul'>;
+type ListItemProps = ComponentPropsWithoutRef<'li'>;
+type AnchorProps = ComponentPropsWithoutRef<'a'>;
+type BlockquoteProps = ComponentPropsWithoutRef<'blockquote'>;
+
+const components = {
+  h1: ({ children, ...props }: HeadingProps) => (
+    <h1 className="font-medium pt-12 mb-0" {...props}>
+      {children}
+    </h1>
+  ),
+  h2: ({ children, ...props }: HeadingProps) => (
+    <h2 className="text-gray-800 font-medium mt-8 mb-3" {...props}>
+      {children}
+    </h2>
+  ),
+  h3: ({ children, ...props }: HeadingProps) => (
+    <h3 className="text-gray-800 font-medium mt-8 mb-3" {...props}>
+      {children}
+    </h3>
+  ),
+  h4: ({ children, ...props }: HeadingProps) => (
+    <h4 className="font-medium" {...props}>
+      {children}
+    </h4>
+  ),
+  p: (props: ParagraphProps) => (
+    <p className="text-gray-800 leading-snug" {...props} />
+  ),
+  ol: (props: ListProps) => (
+    <ol className="text-gray-800 list-decimal pl-5 space-y-2" {...props} />
+  ),
+  ul: (props: ListProps) => (
+    <ul className="text-gray-800 list-disc pl-5 space-y-1" {...props} />
+  ),
+  li: (props: ListItemProps) => <li className="pl-1" {...props} />,
+  em: (props: ComponentPropsWithoutRef<'em'>) => (
+    <em className="font-medium" {...props} />
+  ),
+  strong: (props: ComponentPropsWithoutRef<'strong'>) => (
+    <strong className="font-medium" {...props} />
+  ),
+  a: ({ href, children, ...props }: AnchorProps) => {
+    const className = 'text-blue-500 hover:text-blue-700';
+    if (href?.startsWith('/')) {
+      return (
+        <Link href={href} className={className} {...props}>
+          {children}
+        </Link>
+      );
+    }
+    if (href?.startsWith('#')) {
+      return (
+        <a href={href} className={className} {...props}>
+          {children}
+        </a>
+      );
+    }
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={className}
+        {...props}
+      >
+        {children}
+      </a>
+    );
+  },
+  code: ({ children, ...props }: ComponentPropsWithoutRef<'code'>) => {
+    // const codeHTML = highlight(children as string);
+    return (
+      <code
+        dangerouslySetInnerHTML={{ __html: children?.toString() ?? '' }}
+        {...props}
+      />
+    );
+  },
+  Table: ({ data }: { data: { headers: string[]; rows: string[][] } }) => (
+    <table>
+      <thead>
+        <tr>
+          {data.headers.map((header) => (
+            <th key={crypto.randomUUID()}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.rows.map((row) => (
+          <tr key={crypto.randomUUID()}>
+            {row.map((cell) => (
+              <td key={crypto.randomUUID()}>{cell}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  ),
+  blockquote: (props: BlockquoteProps) => (
+    <blockquote
+      className="ml-[0.075em] border-l-3 border-gray-300 pl-4 text-gray-700"
+      {...props}
+    />
+  ),
+};
+
+export function useMDXComponents(): MDXComponents {
+  return components;
 }
